b4bb4807cef2507fe0b4385b3a02de56140ff442
MANJU4440 <tanegundimanjunath@gmail.com>
2024-07-04 19:14:06 +0530

    updated location

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -16,7 +16,7 @@ function Contactus() {
           <li>Do you have a general question about construction?</li>
           <li>Do you have any upcoming projects you'd like to discuss?</li>
           <li>Are you a subcontractor and would love to partner with buildAhome?</li>
-          <li>Are you enquiring about careers at buildAhome?</li>
+          <li>Are you enquiring about careers at SwaraInfra?</li>
         </ul>
       </section>
       <section className="contact">
@@ -44,7 +44,8 @@ function Contactus() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d605.6396172426017!2d75.11453665467444!3d15.355479937451365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb8d729e11eaac5%3A0x5188820b2489475!2sRidhi%20sidhi%20glass%20hubali!5e0!3m2!1sen!2sin!4v1717178014933!5m2!1sen!2sin"
             width="500" height="300" style={{ border: '0' }} allowFullScreen="" loading="lazy"
           ></iframe>
-          <p>Information about Location </p>
+          <p>Hubballi Office:<br />
+          Shop No 17, second floor , Ridhi Sidhi complex opp, Giants School Akshay park , Gokul Road Hubballi </p>
         </div>
       </section>
     </main>
